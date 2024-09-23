f6d3d11ee5c220005c60a5da616c116bb3d5de6f
Edu <luis_100404@hotmail.com>
2024-09-23 15:41:13 -0300

    projeto files

---
+++ front.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Facul</title>
+    <link rel="stylesheet" href="public/global.css">
+    <link rel="stylesheet" href="public/front.css">
+</head>
+<body>
+
+    <?php include "navbar.php";?>
+
+    <h1>Front</h1>
+    
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Facul</title>
+    <link rel="stylesheet" href="public/navbar.css">
+</head>
+
+<body>
+    
+    <nav>
+        
+        <div>
+            <h1 id="logo">L O G O</h1>
+        </div>
+        
+        <div>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="front.php">Front</a></li>
+                <li><a href="back.php">Back</a></li>
+                <li><a href="full.php">Full</a></li>
+            </ul>
+        </div>
+        
+    </nav>
+
+</body>
